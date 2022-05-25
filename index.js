a3d257ce2ddac0839f1f073e5716626133551786
Ney Roman <romanher_21@hotmail.com>
2022-05-25 17:16:16 -0500

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3001
+const port = 17995
 
 const testRoute = require('./routes/main.routes')
 
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', testRoute);
-
+app.set('port', port)
 app.listen(port, () => console.log('conectado'))
 
 module.exports = {app}
