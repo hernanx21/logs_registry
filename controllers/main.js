2b7baaf78f1479d45bde0c9fa5e059781ed54ed6
Ney Roman <romanher_21@hotmail.com>
2022-05-25 16:19:29 -0500

    deploy to heroku

--- controllers/main.js
+++ controllers/main.js
@@ -11,6 +11,18 @@ const handlers = ({axios}) => ({
             return res.status(201).send(data)
         }
         res.sendStatus(400)
+    },
+    get: async (req,res) => {
+        const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users')
+        //console.log(users)
+        //const found = users.find(r => r.id === req.body.userId)
+        //console.log(found)
+        if(users){
+            return res.status(201).send(users)
+        }else{
+            res.sendStatus(400)
+        }
+        
     }
 })
 
